361a65eda2817d0b4358f7d6ac8bb7201902395b
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-04-08 11:27:51 +0300

    Add bare component logic inside each component

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <div>
+      <h1>Button Component</h1>
+    </div>
+  )
+}
+
+export default Button
---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ButtonPanel() {
+  return (
+    <div>
+      <h1>ButtonPanel Component</h1>
+    </div>
+  );
+}
+
+export default ButtonPanel
