389ddc622865710a38649044eed586925307a23e
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-04-08 12:16:45 +0300

    Implement the ButtonPanel component: add all calculator buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Button({ name }) {
-  console.log(name);
   return (
     <div>
       <button type="button">{name}</button>
