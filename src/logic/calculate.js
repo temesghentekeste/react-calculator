da0581cc963ecae48db750ad349017cabbcc34e9
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-04-09 09:47:55 +0300

    Update calculate.js   - import operate.js   - add basic math logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,4 +1,49 @@
+import operate from './operate';
 export default calculate = (calculatorData, btnName) => {
+  const newCalculatorData = { ...calculatorData };
   const { total, next, operation } = calculatorData;
-  console.log(total, next, operation);
+  switch (btnName) {
+    case 'AC':
+      newCalculatorData.total = null;
+      newCalculatorData.next = null;
+      newCalculatorData.operation = null;
+      break;
+
+    case '=':
+      if (total && next && operation) {
+        newCalculatorData.total = operate(
+          newCalculatorData.total,
+          newCalculatorData.next,
+          newCalculatorData.operation
+        );
+        newCalculatorData.next = null;
+        newCalculatorData.operation = null;
+      }
+      break;
+
+    case '%':
+      if (total && next && operation) {
+        newCalculatorData.total = newCalculatorData.total / 100;
+        newCalculatorData.operation = null;
+      }
+      break;
+
+    case '+/-':
+      newCalculatorData.total *= -1;
+      newCalculatorData.next *= -1;
+      break;
+
+    case data.operation:
+      newCalculatorData.total = operate(
+        newCalculatorData.total,
+        newCalculatorData.next,
+        newCalculatorData.operation
+      );
+      break;
+
+    default:
+      break;
+  }
+
+  return newCalculatorData;
 };
