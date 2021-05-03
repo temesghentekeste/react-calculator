b79c81af42ddcb6bd41f9e28f09f805aeb56be38
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-05-03 11:10:44 +0300

    Refactor App component; convert it to function component with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,32 @@
 /* eslint-disable react/no-unused-state */
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import PropTypes from 'prop-types';
+import { useState } from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import '../App.css';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-      currentDisplay: '0',
-      calculated: false,
-    };
-  }
+const App = () => {
+  const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+    currentDisplay: '0',
+    calculated: false,
+  });
 
-  handleClick = (btnName) => {
-    const currentOperation = calculate(this.state, btnName);
-    this.setState(currentOperation);
+  const handleClick = (btnName) => {
+    const currentOperation = calculate(calculatorData, btnName);
+    setCalculatorData(currentOperation);
   };
 
-  render() {
-    const {
-      currentDisplay,
-    } = this.state;
-    return (
-      <>
-        <Display result={currentDisplay} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
-}
+  const { currentDisplay } = calculatorData;
+  return (
+    <>
+      <Display result={currentDisplay} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
 
 export default App;
