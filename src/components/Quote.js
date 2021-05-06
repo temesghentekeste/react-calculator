d084cb41b8ef825d827e4bf34951b2aba15fb2f3
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-05-06 22:29:21 +0300

    Rename Home to Quote

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Quote = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Quote</h1>
     </div>
   )
 }
 
-export default Home
+export default Quote
