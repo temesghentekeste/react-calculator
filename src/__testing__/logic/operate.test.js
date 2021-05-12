5af485f674d3545db6965e18ca7bb315dd2de7c5
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-05-12 22:58:59 +0300

    Unit test for multiply operation

--- src/__testing__/logic/operate.test.js
+++ src/__testing__/logic/operate.test.js
@@ -21,5 +21,15 @@ describe('calculate operations', () => {
     const sum = operate(5, 100, '-');
     expect(sum).not.toBe('15');
   });
+
+   test('should perform multiplication correctly', () => {
+     const sum = operate(5, 10, 'X');
+     expect(sum).toBe('50');
+   });
+
+   test('should not perform multiplication incorrectly', () => {
+     const sum = operate(5, 100, 'X');
+     expect(sum).not.toBe('15');
+   });
   
 })
