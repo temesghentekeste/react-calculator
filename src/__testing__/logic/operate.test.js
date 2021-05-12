428409dfb9d6efe512c5574e4c3c6a58bcbf3c45
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-05-12 22:57:57 +0300

    Unit test for subtracion operation

--- src/__testing__/logic/operate.test.js
+++ src/__testing__/logic/operate.test.js
@@ -11,5 +11,15 @@ describe('calculate operations', () => {
     const sum = operate(5, 100, '+');
     expect(sum).not.toBe('15');
   });
+
+  test('should perform subtraction correctly', () => {
+    const sum = operate(5, 10, '-');
+    expect(sum).toBe('-5');
+  });
+
+  test('should not perform subtraction incorrectly', () => {
+    const sum = operate(5, 100, '-');
+    expect(sum).not.toBe('15');
+  });
   
 })
