0ef82f9ca847430e9b67b91f52274571ef1d6c91
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-05-13 01:36:23 +0300

    Unit test to render Quote component with correct text

--- src/__testing__/components/Quote.test.js
+++ src/__testing__/components/Quote.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Quote from '../../components/Quote';
 
-it('renders the Quote component', () => {
-  const tree = renderer.create(<Quote />).toJSON();
+let getByTestId;
 
-  expect(tree).toMatchSnapshot();
+beforeEach(() => {
+  const component = render(<Quote/>);
+  getByTestId = component.getByTestId;
+})
+
+it('renders the Quote component', () => {
+  const component = renderer.create(<Quote />).toJSON();
+
+  expect(component).toMatchSnapshot();
+});
+
+test('should render Quote component with correct text', () => {
+  // First we need to render our component using the render function
+  const headerEl = getByTestId('quote');
+
+  expect(headerEl.textContent).toBe('Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. –William Paul Thurston');
 });
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Quote = () => (
   <div>
-    <p className="quoteText">
+    <p className="quoteText" data-testid="quote">
       Mathematics is not about numbers, equations, computations, or algorithms:
       it is about understanding. –William Paul Thurston
     </p>
