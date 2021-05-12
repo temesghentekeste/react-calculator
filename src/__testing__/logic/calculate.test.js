1c01714b9ad9e9c06f437a4c6deacb68498357ce
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-05-12 23:41:31 +0300

    Unit test for should return correct subtraction result  on = btn click

--- src/__testing__/logic/calculate.test.js
+++ src/__testing__/logic/calculate.test.js
@@ -20,4 +20,24 @@ describe('Operations from calculate.js file', () => {
       total: 15,
     });
   });
+
+  test('should return correct subtraction operation on = btn click', () => {
+    const calculatorData = {
+      calculated: false,
+      currentDisplay: '3',
+      next: '3',
+      operation: '-',
+      total: '12',
+    };
+
+    const result = calculate(calculatorData, '=');
+
+    expect(result).toStrictEqual({
+      calculated: true,
+      currentDisplay: '9',
+      next: '0',
+      operation: null,
+      total: 9,
+    });
+  });
 });
