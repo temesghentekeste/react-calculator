39f65bf903f8a187bd0f0bf2cb93d1e082bdec21
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-05-12 22:55:28 +0300

    More unit test for addition operation

--- src/__testing__/logic/operate.test.js
+++ src/__testing__/logic/operate.test.js
@@ -6,5 +6,10 @@ describe('calculate operations', () => {
     const sum = operate(5, 10, '+')
     expect(sum).toBe('15')
   })
+
+  test('should not perform addition incorrectly', () => {
+    const sum = operate(5, 100, '+');
+    expect(sum).not.toBe('15');
+  });
   
 })
