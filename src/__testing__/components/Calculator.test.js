e0d01594ff63f61ba9f07793507ec2aa0dbdd4b2
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-05-14 11:31:27 +0300

    Remove unncessary imports

--- src/__testing__/components/Calculator.test.js
+++ src/__testing__/components/Calculator.test.js
@@ -3,9 +3,6 @@ import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect';
 import Calculator from '../../components/Calculator';
-import ButtonPanel from '../../components/ButtonPanel';
-import Display from '../../components/Display';
-import calculate from '../../logic/calculate';
 
 let getByTestId;
 
