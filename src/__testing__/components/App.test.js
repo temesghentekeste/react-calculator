af28186b3b41aefdd1111bb70b4f40797fe282f3
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-05-14 12:09:53 +0300

    Unit test for App component to include Lorem ipsum text

--- src/__testing__/components/App.test.js
+++ src/__testing__/components/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from '../../components/App';
 
@@ -17,4 +17,21 @@ it('renders the App component', () => {
   expect(component).toMatchSnapshot();
 });
 
+test('should render App component with correct heading text', () => {
+  // First we need to render our component using the render function
+  const headerEl = getByTestId('mainHeading');
 
+  expect(headerEl.textContent).toBe('Welcome to our page!');
+});
+
+it('renders the App component without AC text', () => {
+  render(<App />);
+
+  expect(screen.queryByText('AC')).toBeFalsy();
+});
+
+it('renders the App component with Lorem ipsum  text', () => {
+  const mainText = getByTestId('mainText');
+
+  expect(mainText.textContent.includes('Lorem ipsum')).toBe(true);
+});
